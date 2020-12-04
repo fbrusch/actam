2363f0f8326d132cde564d456dc5f00df9a88dcb
fbrusch <fbrusch@gmail.com>
2020-12-04 16:13:25 +0100

    Play with midi

--- code/midi/index.js
+++ code/midi/index.js
@@ -1,1 +1,43 @@
 
+
+
+var synth = new Tone.Synth()
+
+window.synth = synth
+
+var delay = new Tone.Delay()
+var gain = new Tone.Gain();
+gain.gain.value = 0.4;
+delay.delayTime.value = 0.3;
+synth.connect(delay);
+synth.toDestination();
+
+delay.toDestination();
+delay.connect(gain);
+gain.connect(delay);
+
+var midi = navigator.requestMIDIAccess().then(function(midi) {
+    for(i of midi.inputs.values()) {
+        i.onmidimessage = function(event) {
+            if(event.data[0] == 144)
+                synth.triggerAttackRelease(Math.pow(2,(event.data[1]-45)/12)*440,"8n"); 
+            else if(event.data[0] == 191 && event.data[1] == 80) {
+                if(event.data[2] == 1) {
+                    delay.delayTime.value += 0.05;
+                } else {
+                    delay.delayTime.value -= 0.05;
+                }
+            } else if(event.data[0] == 191 && event.data[1] == 81) {
+                if(event.data[2] == 1) {
+                    gain.gain.value += 0.05;
+                } else {
+                    gain.gain.value -= 0.05;
+                }
+            }
+            console.log(event.data)
+
+        }
+    }
+})
+
+
