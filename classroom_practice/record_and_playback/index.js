eb8477e28352bc1e7fe7b80304ea8b6fba47a303
francesco <francesco@MacBook-Air-di-francesco.local>
2021-11-17 15:35:39 +0100

    complete recording section

--- classroom_practice/record_and_playback/index.js
+++ classroom_practice/record_and_playback/index.js
@@ -2,12 +2,31 @@ const c = new AudioContext();
 
 const recording_length = 10;
 
-var b = c.createBuffer(1, recording_length*c.sampleRate,
+var recordBuffer = c.createBuffer(1, recording_length*c.sampleRate,
     c.sampleRate);
+
+var bindex = 0;
+
+var pn;
 
 async function main() {
     var stream = await navigator.mediaDevices.getUserMedia({audio:true})
-    //...
+    var mss = c.createMediaStreamSource(stream);
+    //mss.connect(c.destination)
+    pn = c.createScriptProcessor(1024, 1,1);
+    pn.onaudioprocess = function(event) {
+        const dataIn = event.inputBuffer.getChannelData(0);
+        var recordBufferData = recordBuffer.getChannelData(0);
+        for(var i=0;i < dataIn.length; i++) {
+            recordBufferData[bindex++ % recordBuffer.length] = 
+                dataIn[i]
+        }
+        
+
+    }
+    mss.connect(pn)
+    pn.connect(c.destination)
+
 
 }
 
